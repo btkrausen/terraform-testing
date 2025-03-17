5f55a6c0858edc0997397ff8595fc3cad9f8d849
Bryan Krausen <bkrausen@riverpointtechnology.com>
2025-03-17 13:52:56 -0400

    purposely breaking change

--- modules/tfb/aws/lab_03/main.tf
+++ modules/tfb/aws/lab_03/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dn   = true
 
   tags = {
     Name        = "terraform-course"
