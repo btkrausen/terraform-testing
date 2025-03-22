e9b8b95d4b2cf8c90d4e70f8255da224fabf1088
Bryan Krausen <bkrausen@riverpointtechnology.com>
2025-03-22 13:29:17 -0400

    add lab 14

---
+++ modules/tfb/github/lab_14/main.tf
@@ -0,0 +1,39 @@
+module "repository" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  name        = "${var.environment}-app"
+  description = "Application repository for ${var.environment} environment"
+  topics      = ["terraform", "application", var.environment]
+  visibility  = var.repository_visibility
+  archived    = false
+}
+
+module "default-branch-protection" {
+  source  = "masterborn/default-branch-protection/github"
+  version = "1.1.0"
+
+  repository_name = split("/", module.repository.full_name)[1]
+}
+
+
+
+module "user-repositories" {
+  source  = "mineiros-io/repository/github"
+  version = "0.18.0"
+
+  for_each    = var.user_repos
+  name        = "${each.value}-${var.environment}-app"
+  description = "Application repository for ${var.environment} environment"
+  topics      = ["terraform", "application", var.environment]
+  visibility  = var.repository_visibility
+  archived    = false
+}
+
+module "user-default-branch-protection" {
+  source  = "masterborn/default-branch-protection/github"
+  version = "1.1.0"
+
+  for_each = var.user_repos
+  repository_name = split("/", module.user-repositories[each.key].full_name)[1]
+}
