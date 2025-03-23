10642b441b5f29eca551621404b8ef9d90c08ebb
Bryan Krausen <bkrausen@riverpointtechnology.com>
2025-03-23 14:25:38 -0400

    remove duplicate network block

--- modules/tfb/azure/lab_02/main.tf
+++ modules/tfb/azure/lab_02/main.tf
@@ -15,24 +15,11 @@ resource "azurerm_virtual_network" "main" {
   name                = "terraform-network"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["192.168.0.0/16"]
 
   tags = {
     Name        = "terraform-course"
     Environment = "Lab"
-    Managed_By  = "Terraform"
-  }
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "terraform-network"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  address_space       = ["192.168.0.0/16"] # <-- change IP Address
-
-  tags = {
-    Name        = "terraform-course"
-    Environment = "learning-terraform"
     Managed_By  = "Terraform"
   }
 }
