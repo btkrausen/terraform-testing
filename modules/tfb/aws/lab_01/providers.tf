c5f86bbd9d5b6407fdce1146fb7c15e84df3a689
Bryan Krausen <30417793+btkrausen@users.noreply.github.com>
2025-11-09 08:48:28 -0500

    Update comment for required Terraform version

--- modules/tfb/aws/lab_01/providers.tf
+++ modules/tfb/aws/lab_01/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.10.0" # Replace with your installed version
+  required_version = ">= 1.10.0" # Replace with your installed version of Terraform
   required_providers {
     aws = {
       source  = "hashicorp/aws"
